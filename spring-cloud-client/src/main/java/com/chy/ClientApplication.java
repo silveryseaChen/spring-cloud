1fecc6eb30bbe9db66febeda3ffa9e6faf3aa601
chenhaiyin <chenhaiyin@houbank.com>
2018-10-13 12:03:21 +0800

    配置中心通过调用/actuator/bus-refresh 即可更新配置，无需重启。

--- spring-cloud-client/src/main/java/com/chy/ClientApplication.java
+++ spring-cloud-client/src/main/java/com/chy/ClientApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@RefreshScope
 @Slf4j
 public class ClientApplication {
 
